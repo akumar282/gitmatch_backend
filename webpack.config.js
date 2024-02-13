76e6a94c6c31af880c6f3b4d2a6ed03ab0c33e23
akumar282 <abhi.kumar7567@gmail.com>
2024-02-12 23:49:12 -0800

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  target: 'node',
+  devtool: 'inline-source-map',
+  // Multiple entry points for each Lambda function
+  entry: {
+    matchNumberHandler: './lib/src/lambda/matchNumberHandler.ts',
+    projectMatchingHandler: './lib/src/lambda/projectMatchingHandler.ts'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js', // Output each bundle with its entry name
+    libraryTarget: 'commonjs'
+  },
+  resolve: {
+    extensions: ['.ts', '.js'], // Add '.ts' and '.js' as resolvable extensions.
+    alias: {
+      '@lib': path.resolve(__dirname, 'lib/') // Adjust this path as necessary.
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env', // For JavaScript
+              '@babel/preset-typescript' // For TypeScript
+            ]
+          }
+        }
+      }
+    ]
+  },
+  optimization: {
+    minimize: false
+  }
+};
